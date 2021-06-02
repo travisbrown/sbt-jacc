4fd91137fd3c44fb005ad632da1f5f86b684763c
Travis Brown <travisrobertbrown@gmail.com>
2021-06-02 13:16:12 +0200

    Format

--- src/main/scala/dev/travisbrown/jacc/JaccSymbol.scala
+++ src/main/scala/dev/travisbrown/jacc/JaccSymbol.scala
@@ -1,8 +1,15 @@
 package dev.travisbrown.jacc
 
-/** Represents a symbol in a jacc grammar.
+/**
+ * Represents a symbol in a jacc grammar.
  */
-case class JaccSymbol(name: String, num: Int, tokenNo: Int, fixity: Option[Fixity], tpe: Option[String], prods: Array[JaccProd]) {
+case class JaccSymbol(name: String,
+                      num: Int,
+                      tokenNo: Int,
+                      fixity: Option[Fixity],
+                      tpe: Option[String],
+                      prods: Array[JaccProd]
+) {
   def getType: String = tpe.orNull
 
   override def toString(): String = name
